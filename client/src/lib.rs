c1ae6b012099cec83d3f687e66fc8ff77477f319
Jdender <jdenderplays@gmail.com>
2019-04-28 15:12:04 -0400

    Start webgl

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,11 +1,17 @@
 use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
+use wasm_bindgen::JsCast;
+use web_sys::WebGl2RenderingContext;
 
 #[wasm_bindgen(start)]
-pub fn greet() {
-    alert(&format!("Hello, {}!", "Bob"));
+pub fn main() -> Result<(), JsValue> {
+
+    let document = web_sys::window().unwrap().document().unwrap();
+
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+          
+    let context = canvas.get_context("webgl2")?.unwrap()
+        .dyn_into::<WebGl2RenderingContext>()?;
+
+    Ok(())
 }
