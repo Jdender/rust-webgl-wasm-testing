958736a5bee103fcd4582f78cc1cde5603d685cd
Jdender~ <jdenderplays@gmail.com>
2019-04-29 17:48:46 -0400

    Use program_info module and import shaders into lib.rs

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,8 +1,15 @@
 extern crate console_error_panic_hook;
 
+mod program_info;
+use program_info::ProgramInfo;
+
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::WebGl2RenderingContext;
+
+// This can be changed latter
+static FRAG_SHADER: &'static str = include_str!("shaders/fragment.glsl");
+static VERT_SHADER: &'static str = include_str!("shaders/vertex.glsl");
 
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
@@ -16,6 +23,8 @@ pub fn main() -> Result<(), JsValue> {
           
     let context = canvas.get_context("webgl2")?.unwrap()
         .dyn_into::<WebGl2RenderingContext>()?;
+
+    let program_info = ProgramInfo::new(&context, FRAG_SHADER, VERT_SHADER)?;
 
     Ok(())
 }
